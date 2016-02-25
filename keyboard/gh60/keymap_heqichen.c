9496aa13afdf76babefedd3e8fb5fbefcf23c910
HE Qichen <heqichen@gmail.com>
2016-02-26 01:42:51 +0800

    fn layer done

--- keyboard/gh60/keymap_heqichen.c
+++ keyboard/gh60/keymap_heqichen.c
@@ -7,37 +7,37 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------|
      * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |
      * |-----------------------------------------------------------|
-     * |Caps  |  A|  S|  D|  F|  G|  H|  J|  K|  L| ; |  '|Return  |
+     * |Caps  |  A|  S|  D|  F|  G|  H|  J|  K|  L| ; |  '|Enter  |
      * |-----------------------------------------------------------|
      * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |PUp|
      * |-----------------------------------------------------------|
-     * |LCtl| Fn |Alt|      Space             | Alt| Win | Fn | PDn|
+     * |LCtl| Fn |Alt|      Space             | Alt| Win | Fn |RCtl|
      * `-----------------------------------------------------------'
      */
     KEYMAP_HHKB(
         ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, DEL, \
         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,      \
-        LCAP,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,       \
+        CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,       \
         LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,     RSFT,PGUP,       \
-        LCTL,FN0, LALT,          SPC,                     RALT,RGUI,FN0, PGDN),
+        LCTL,FN0, LALT,          SPC,                     RALT,RGUI,FN0, RCTL),
     /* 1: HHKB Fn layer
      * ,-----------------------------------------------------------.
-     * |Pwr| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del|
+     * |`  | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Bsp|Del|
      * |-----------------------------------------------------------|
-     * |Caps |   |   |   |   |   |   |   |Psc|Slk|Pus|Up |   |     |
+     * |     |MWU|MUp |MWD|   |   |   |Hom|End|PgU|PgD|   |   |    |
      * |-----------------------------------------------------------|
-     * |      |VoD|VoU|Mut|Ejc|   |  *|  /|Hom|PgU|Lef|Rig|Enter   |
+     * |      |MLe|MDo|MRi|VoD|VoU|Lef|Dow|Up |Rig|   |   |        |
      * |-----------------------------------------------------------|
-     * |        |   |   |   |   |   |  +|  -|End|PgD|Dow|      |   |
+     * |        |MLB|MMB|MRB|   |   |   |   |Bsp|Del|   |      |   |
      * |-----------------------------------------------------------|
      * |    |    |    |                        |    |    |    |    |
      * `-----------------------------------------------------------'
      */
     KEYMAP_HHKB(
-        PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL,  \
-        CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS,UP,  TRNS,TRNS,      \
-        TRNS,VOLD,VOLU,MUTE,EJCT,TRNS,PAST,PSLS,HOME,PGUP,LEFT,RGHT,     PENT,      \
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,     TRNS,TRNS,      \
+        GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, TRNS,TRNS,  \
+        TRNS,WH_U,MS_U,WH_D,TRNS,TRNS,TRNS,HOME,END ,PGUP,PGDN,TRNS,TRNS,TRNS,      \
+        TRNS,MS_L,MS_D,MS_R,_VOLDOWN,_VOLUP,LEFT,DOWN,UP  ,RIGHT,TRNS,TRNS,    TRNS,      \
+        TRNS,BTN1,BTN3,BTN2,TRNS,TRNS,TRNS,TRNS,BSPC,DEL, TRNS,     TRNS,TRNS,      \
         TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS),
 
     /* 0: Default layer
