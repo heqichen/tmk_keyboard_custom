ffb25bab6f6f4d91d87378d355c47d1c14aa47ef
HE Qichen <heqichen@gmail.com>
2017-02-08 04:02:28 +0800

    add another layer of fn key

--- keyboard/gh60/keymap_heqichen.c
+++ keyboard/gh60/keymap_heqichen.c
@@ -9,7 +9,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------|
      * |Caps  |  A|  S|  D|  F|  G|  H|  J|  K|  L| ; |  '|Enter  |
      * |-----------------------------------------------------------|
-     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |PUp|
+     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |FN1|
      * |-----------------------------------------------------------|
      * |LCtl| Fn |Alt|      Space             | Win|  |Alt Fn |RCtl|
      * `-----------------------------------------------------------'
@@ -18,10 +18,9 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, DEL, \
         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,      \
         CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,       \
-        LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,     RSFT,PGUP,       \
+        LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,     RSFT,FN1,       \
         LCTL,FN0, LALT,          SPC,                     RGUI,RALT,FN0, RCTL),
-    /* 1: HHKB Fn layer
-     * ,-----------------------------------------------------------.
+     /*,-----------------------------------------------------------.
      * |`  | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Bsp|Del|
      * |-----------------------------------------------------------|
      * |     |MWU|MUp |MWD|   |   |   |Hom|End|PgU|PgD|   |   |    |
@@ -33,17 +32,26 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |    |    |    |                        |    |    |    |    |
      * `-----------------------------------------------------------'
      */
+
     KEYMAP_HQC(
         GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, TRNS,TRNS,  \
         TRNS,WH_U,MS_U,WH_D,TRNS,TRNS,TRNS,HOME,END ,PGUP,PGDN,TRNS,TRNS,TRNS,      \
-        TRNS,MS_L,MS_D,MS_R,_VOLDOWN,_VOLUP,LEFT,DOWN,UP  ,RIGHT,TRNS,TRNS,    TRNS,      \
-        TRNS,BTN1,BTN3,BTN2,WBAK,WFWD,LBRC,RBRC,BSPC,DEL, TRNS,     TRNS,TRNS,      \
-        TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS)
+        TRNS,MS_L,MS_D,MS_R,_VOLDOWN,_VOLUP,LEFT,DOWN,UP ,RIGHT,TRNS,TRNS,    TRNS,      \
+        TRNS,BTN1,BTN3,BTN2,WBAK,WFWD,LBRC,RBRC,BSPC,DEL, TRNS,     TRNS,FN1,      \
+        TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS),
+    //FN1
+    KEYMAP_HQC(
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,  \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,      \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,    TRNS,      \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,      UP ,FN1,      \
+        TRNS,TRNS,TRNS,          TRNS,                    TRNS,LEFT,DOWN,RIGHT)
 };
 
 
 const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_MOMENTARY(1),  // to Fn overlay
+    [1] = ACTION_LAYER_MOMENTARY(2),  // to Fn overlay
 };
 
 #ifdef KEYMAP_IN_EEPROM_ENABLE
